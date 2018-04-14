182c4424cab2c2de4841cc4f4dfe1872d3bd7657
EC2 Default User <ec2-user@ip-172-31-25-39.us-east-2.compute.internal>
2018-04-14 16:25:08 +0000

    Treure vots form

--- db/migrate/20180414161450_create_contributions.rb
+++ db/migrate/20180414161450_create_contributions.rb
@@ -4,7 +4,7 @@ class CreateContributions < ActiveRecord::Migration[5.1]
       t.string :title
       t.string :url
       t.text :text
-      t.integer :votes
+      t.integer :votes, :default => 0
       t.references :user, foreign_key: true
 
       t.timestamps
