62b15872c4c62d2256ed109f70ff5bfcf0635eab
EC2 Default User <ec2-user@ip-172-31-12-196.us-east-2.compute.internal>
2018-06-02 21:08:53 +0000

    cors headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
-  before_filter :set_headers
   
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -9,11 +8,4 @@ class ApplicationController < ActionController::Base
   end
 
 
-  def set_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Expose-Headers'] = 'ETag'
-    headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
-    headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
-    headers['Access-Control-Max-Age'] = '86400'
-  end
 end
